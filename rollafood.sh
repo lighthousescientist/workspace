37202b53ffae96198a092df2eaf610b899fd4b0e
Will McArthur <williammcarthur@hotmail.ca>
2016-10-03 14:32:48 +0000

    Week 5

--- rollafood.sh
+++ rollafood.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-#Create an array of 11 food names
-#Create 2 vars with random numbers in them from 1-6 each
-#Add the two numbers together and use the sum as an index to display a food from array
+#Create an array containing 11 different food names. 
+#Create two variables with random numbers in them from 1-6 each. 
+#Add the two numbers together and use the sum as an index to 
+#display a food from your array
 
-ARRAY=(apple banana cucumber doritos eggroll fajita greenpepper jell-o lambchop onion porkchop)
-random1=$(( ( RANDOM % 6 ) + 1))
-random2=$(( ( RANDOM % 6 ) + 1))
-index=$(( $random1+$random2 ))
-
-echo ${ARRAY[$index-1]}
+# create array of foods
+foods=(apple banana pizza wings beer steak sandwich "pop tart" chicken ribs dirt)
+# roll 2 standard six-sided dice
+die1=$(($RANDOM %6))
+die2=$(($RANDOM %6))
+# the food index is 0-10
+foodindex=$((die1 + die2))
+# for dicetotal, have to add 2 because the remainders are in the range of 0-5 and dice go 1-6
+dicetotal=$((die1 + die2 + 2))
+# look up the data first to make the output command more readable
+food=${foods[$foodindex]}
+# everything done, produce output
+echo "Yum, I rolled $dicetotal which gives me $food!"
 
 
