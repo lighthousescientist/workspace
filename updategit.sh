502fa4596163f76d6e29476a347406cd30960fb9
Will McArthur <williammcarthur@hotmail.ca>
2016-10-03 14:35:14 +0000

    Week 5

--- updategit.sh
+++ updategit.sh
@@ -6,6 +6,8 @@
 #should at least be checking if $1 has anything in it
 #read -p "Update message? " message
 
+chmod u+x ~/workspace/*.sh
+ln -s ~/workspace/*.sh ~/workspace/bin/
 git add -A
 git commit -m "$1"
 git push origin master
