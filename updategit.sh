949f14fefb1aab492a6d57d61a9e66be53c980b4
Will McArthur <williammcarthur@hotmail.ca>
2016-10-03 13:55:00 +0000

    Week 5

---
+++ updategit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#this script saves me from remembering git commands
+
+#this would be better than blindly using $1
+#should at least be checking if $1 has anything in it
+#read -p "Update message? " message
+
+git add -A
+git commit -m "$1"
+git push origin master
+
