cb51f8c48a1b3991e0a696529b883d272efaa661
Will McArthur <williammcarthur@hotmail.ca>
2016-10-14 17:27:27 +0000

    Did some work on lab4

--- lab4/lab4rolldice.sh
+++ lab4/lab4rolldice.sh
@@ -11,3 +11,53 @@
 #line options, only asking the user for those numbers if they didn't give them 
 #on the command line. Also accept a help option (-h or --help) that shows command 
 #syntax. Use a function to display command syntax help when appropriate.
+
+#lab4#######################
+while getopts ":d:s:h:" opt; do
+  case $opt in
+    d)
+      echo "# of dice"
+      let dice=${OPTARG}
+      #[ -z ${OPTARG} ] || let dice=2
+      echo $dice
+      ;;
+    s)
+      echo "# of sides"
+      let sides=${OPTARG}
+      #[ -z ${OPTARG} ] || let sides=6
+      echo $sides
+      exit
+      ;;
+    h)
+      echo "-d      dice count (1-5) (default 2)"
+      echo "-s      side count (4-20) (default 6)"
+      exit
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit
+      ;;
+  esac
+done
+############################
+
+read -p "Provide a dice count (1-5) (default 2): " dice
+[ -n "$dice" ] || let dice=2 
+echo $dice
+
+read -p "How many sides should dice have (4-20) (default 6): " sides
+[ -n "$sides" ] || let sides=6
+echo $sides
+
+echo "Rolling virtual dice..."
+
+i="0"
+while [ $i -lt $dice ]
+do
+roll=$(($RANDOM %$sides+1))
+echo Rolled a $roll
+total=$(($total + $roll))
+i=$[$i+1]
+done
+echo For a total of $total.
+
