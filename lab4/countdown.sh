85573190f4940f948195ea4deb87a7ca7b7b7845
Will McArthur <williammcarthur@hotmail.ca>
2016-11-04 04:02:13 +0000

    Adding notes

--- lab4/countdown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Refer to presentation.
-#Allow the user to specify the countdown seconds on the command line. 
-#Use a help function and your error-message function. Use functions for your signal processing.
-
-#SIGINT = ^C
-#SIGQUIT = ^\
